d909311d19fd48b78662773984d3bcc725bd56e6
Oliver Wright <obwan02@hotmail.co.nz>
2019-05-15 09:22:53 +1200

    Added linux startup functionality.

--- startup/core.py
+++ startup/core.py
@@ -1,8 +1,10 @@
 import platform
 from startup import operating_systems
 
+os_startups = { 'Windows': operating_systems.Windows().add, 'Linux': operating_systems.Linux().add }
+
 def add(script_location):
-    if platform.system() == 'Windows':
-        operating_systems.Windows().add(script_location)
-    else:
-        raise OSError('This operating system is not supported')
+    try:
+        os_startups[platform.system()]
+    except KeyError:
+        raise NotImplementedError('OS Not Supported')
